946e8b3acfdb4135a56efb0b180ea25c9d4379c8
8ar7ar <5ar7ar@mail.ru>
2023-11-14 08:17:04 +0500

    stage1-module6-io-task1 "1st attempt, locally tested"

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,40 @@
 package com.epam.mjc.io;
 
-import java.io.File;
+import java.io.*;
 
 
 public class FileReader {
+    public File file;
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        this.file = file;
+        String[] userTokens = ReadFileDataIntoString(file);
+
+        String name = userTokens[0];
+        int age = Integer.parseInt(userTokens[1]);
+        String email = userTokens[2];
+        long phone = Long.parseLong(userTokens[3]);
+
+        return new Profile(name, age, email, phone);
+    }
+
+    public String[] ReadFileDataIntoString(File file){
+
+        StringBuilder strBuilder = new StringBuilder();
+        String rawData = "";
+        try (FileInputStream inputStream = new FileInputStream(file)) {
+            int ch;
+            while ((ch = inputStream.read()) != -1) strBuilder.append((char)ch);
+            rawData = strBuilder.toString();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        String[] keyValuePairs = rawData.split("\n");
+        String[] userProfileFields = new String[keyValuePairs.length];
+        for (int i = 0; i < keyValuePairs.length; i++) {
+            userProfileFields[i] = keyValuePairs[i].split(": ")[1];
+        }
+        return userProfileFields;
     }
 }
